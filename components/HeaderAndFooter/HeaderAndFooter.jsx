52561378e4e5530febab13613d7dc6cabcf06f9b
alireza <alireza_physicist@yahoo.com>
2022-01-28 21:02:22 +0330

    add fonts and header and footer

---
+++ components/HeaderAndFooter/HeaderAndFooter.jsx
@@ -0,0 +1,11 @@
+const HeaderAndFooter = ({children}) => {
+    return (
+        <>
+            <h1> هدر فروشگاه اینترنتی اسنپ مارکت </h1>
+            {children}
+            <h2> فوتر فروشگاه اینترنتی اسنپ مارکت </h2>
+        </>
+    )
+}
+
+export default HeaderAndFooter
