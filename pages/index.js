6ef9bea6fe5783f1165636f41981e76e54165900
alireza <alireza_physicist@yahoo.com>
2022-01-29 11:52:10 +0330

    merged with the last changes of Abolfazl. fonts and direction set.

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,20 @@
 import Head from "next/head";
 import styles from '../styles/Home.module.css'
 import {useEffect} from "react";
-// import ProductSingle from "../components/ProductCard/ProductCard";
+import ProductSingle from "../components/ProductCard/ProductCard";
 
 
 export default function Home() {
 
-  // const imagepack = [
-  //   {
-  //     src:'https://api.snapp.market/media/cache/product-variation_image2/uploads/images/vendors/users/app/60cf1e4112f33.jpg'
-  //   },{
-  //     src:'https://api.snapp.market/media/cache/product-variation_image2/uploads/images/vendors/users/app/60cf1e4112f33.jpg'
-  //   },{
-  //     src:'https://api.snapp.market/media/cache/product-variation_image2/uploads/images/vendors/users/app/60cf1e4112f33.jpg'
-  //   }
-  // ] ;
+  const imagepack = [
+    {
+      src:'https://api.snapp.market/media/cache/product-variation_image2/uploads/images/vendors/users/app/60cf1e4112f33.jpg'
+    },{
+      src:'https://api.snapp.market/media/cache/product-variation_image2/uploads/images/vendors/users/app/60cf1e4112f33.jpg'
+    },{
+      src:'https://api.snapp.market/media/cache/product-variation_image2/uploads/images/vendors/users/app/60cf1e4112f33.jpg'
+    }
+  ] ;
 
   useEffect(() => {
     fetch("/api/products")
@@ -29,21 +29,21 @@ export default function Home() {
           <meta name='keywords' content='marketing site, developed with nextJs'/>
         </Head>
         <h4>پروژه اسنپ مارکت</h4>
-        {/*<ProductSingle*/}
-        {/*    src='https://api.snapp.market/media/cache/product-variation_image2/uploads/images/vendors/users/app/60cf1e4112f33.jpg'*/}
-        {/*    width='14.1rem'*/}
-        {/*    ordinary*/}
-        {/*/>*/}
-        {/*<ProductSingle*/}
-        {/*    src='https://api.snapp.market/media/cache/product-variation_image2/uploads/images/vendors/users/app/60cf1e4112f33.jpg'*/}
-        {/*    width='14.1rem'*/}
-        {/*    special*/}
-        {/*/>*/}
-        {/*<ProductSingle*/}
-        {/*    width='14.1rem'*/}
-        {/*    imgPack = {imagepack}*/}
-        {/*    goldenOffer*/}
-        {/*/>*/}
+        <ProductSingle
+            src='https://api.snapp.market/media/cache/product-variation_image2/uploads/images/vendors/users/app/60cf1e4112f33.jpg'
+            width='14.1rem'
+            ordinary
+        />
+        <ProductSingle
+            src='https://api.snapp.market/media/cache/product-variation_image2/uploads/images/vendors/users/app/60cf1e4112f33.jpg'
+            width='14.1rem'
+            special
+        />
+        <ProductSingle
+            width='14.1rem'
+            imgPack = {imagepack}
+            goldenOffer
+        />
       </div>
   )
 }
