2c537c3469324722ba6cf6a307cdff0e92174d2b
kimia takht sangi <kimi.ts.1998@gmail.com>
2022-02-08 15:53:22 +0330

    edit iApps Tooltip

--- components/Footer/iAppsTooltip.jsx
+++ components/Footer/iAppsTooltip.jsx
@@ -7,6 +7,9 @@ import Typography from '@mui/material/Typography';
 import ClickAwayListener from '@mui/material/ClickAwayListener';
 import Button from '@mui/material/Button';
 import Stack from '@mui/material/Stack';
+import {createTheme,ThemeProvider} from'@mui/material';
+import Box from '@mui/material/Box';
+
 
 const HtmlTooltip = styled(({ className, ...props }) => (
     <Tooltip {...props} classes={{ popper: className }} />
@@ -17,11 +20,34 @@ const HtmlTooltip = styled(({ className, ...props }) => (
   color: 'rgba(0, 0, 0, 0.87)',
   boxShadow:'rgb(0 0 0 / 16%) 0px 2px 8px 0px',
   borderRadius: '0.5rem',
-  border: '0.5px solid rgb(230, 230, 230)',
-  width: 'max-content'
+  border: '1px solid rgb(230, 230, 230)',
+  width: 'max-content',
+  height:40
  
 },
 }));
+const theme = createTheme({
+  components:{
+    MuiButton:{
+      variants:[
+        {
+          props:{
+            variant:"contained"
+          },
+          style:{
+            width:110,
+            height:30,
+            backGround:'#052cf00f',
+            color:'#052cf0',
+            fontSize:'12px',
+            fontWeight:500
+          }
+        }
+      ]
+
+    }
+  }
+})
 
 export default function TriggersTooltips() {
         const [open, setOpen] = React.useState(false);
@@ -36,20 +62,21 @@ export default function TriggersTooltips() {
 return (
     <ClickAwayListener onClickAway={handleTooltipClose}>
         <div>
-            <HtmlTooltip  placement="top"   arrow  PopperProps={{disablePortal: true,}} onClose={handleTooltipClose} open={open}
+            <HtmlTooltip  placement="top"   arrow  PopperProps={{disablePortal: true,}} onClose={handleTooltipClose} open={open} disableFocusListener disableHoverListener disableTouchListener
               title={
-                <Stack direction="row" spacing={5} >
-                    {"50% کد اشتراک تخفیف با"}
-                <Typography color="blue">ism</Typography>
-                  <Button variant="contained" sx={{width:110,height:30,background:'#052cf00f', color:'#052cf0', fontSize:'12px',marginRight:5}} href='https://iapps.ir/app/%D8%A7%D8%B3%D9%86%D9%BE-%D9%85%D8%A7%D8%B1%DA%A9%D8%AA-Snapp-Market/57404100'>
+              <Stack direction="row" spacing={2} >
+                <Box     sx={{display: 'flex',alignItems: 'flex-start',flexDirection: 'column'}}>
+                      {"50% کد اشتراک تخفیف با"}
+                      <Typography variant='body3' color="blue">ism</Typography>
+                </Box>
+                  <ThemeProvider>
+                    <Button  href='https://iapps.ir/app/%D8%A7%D8%B3%D9%86%D9%BE-%D9%85%D8%A7%D8%B1%DA%A9%D8%AA-Snapp-Market/57404100'>
                       دریافت از آی اپس
-                  </Button>
-                  
-                </Stack>
-              }>
-                        <img onClick={handleTooltipOpen} style={{width:125,height:40,margin:5}}   src='https://snapp.market/v2/static/images/b73e78b6652ebf0b5c4034fd17b91933.png' alt=''></img>
-
-                  </HtmlTooltip>   
+                    </Button>
+                  </ThemeProvider>
+              </Stack>}>
+                  <img onClick={handleTooltipOpen} style={{width:125,height:40,margin:5}} src='https://snapp.market/v2/static/images/b73e78b6652ebf0b5c4034fd17b91933.png' alt=''></img>
+            </HtmlTooltip>   
         </div>
     </ClickAwayListener>
 );
