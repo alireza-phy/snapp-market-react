c2b19f0e242a1c5c66c5480fbf5c467c87a15513
alireza <alireza_physicist@yahoo.com>
2022-02-09 15:15:26 +0330

    some naming (milk fatty) was changed in product data

--- components/ProductData/ProductData.js
+++ components/ProductData/ProductData.js
@@ -215,7 +215,7 @@ const ProductData = [
         },
         seller: 'هایپر استار',
         tags: [
-            {id: 1, name: 'پر چرب'},
+            {id: 1, name: 'پرچرب'},
             {id: 2, name: 'کاکائو'}
         ],
         price: 17500,
@@ -707,7 +707,7 @@ const ProductData = [
         },
         seller: 'هایپر استار',
         tags: [
-            {id: 1, name: 'پر چرب'}
+            {id: 1, name: 'پرچرب'}
         ],
         price: 33200,
         discount: null,
