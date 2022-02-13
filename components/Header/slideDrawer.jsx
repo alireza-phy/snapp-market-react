05af74e506a4eef55a99fab85001a91d00825334
alireza <alireza_physicist@yahoo.com>
2022-02-13 14:25:18 +0330

    solve the problems in data at slideDrawer

--- components/Header/slideDrawer.jsx
+++ components/Header/slideDrawer.jsx
@@ -2,7 +2,7 @@ import {Drawer, List, ListItem, ListItemText, Box, Typography} from "@mui/materi
 import {Close, CloseSharp, Lens} from "@mui/icons-material";
 import {useState} from "react";
 import {useTheme} from "@mui/material/styles";
-import {AssortmentCardData , getSubCategory} from '../ProductData/ProductData'
+import {AssortmentCardData, getSubCategory} from '../ProductData/ProductData'
 
 const SlideDrawer = (props) => {
     const {open, onClose} = props;
@@ -29,24 +29,54 @@ const SlideDrawer = (props) => {
             }}
 
             anchor="top" open={open} onClose={() => onClose(false)}>
-            <Box sx={{ display:"flex",height:120,justifyContent:"space-between",padding:"1rem"}}>
-                <Close sx={{color:"gray"}}/>
-                <Typography sx={{fontSize:'1.2rem', fontWeight:700,}}>دسته بندی ها</Typography>
+            <Box sx={{display: "flex", height: 120, justifyContent: "space-between", padding: "1rem"}}>
+                <Close sx={{color: "gray"}}/>
+                <Typography sx={{fontSize: '1.2rem', fontWeight: 700,}}>دسته بندی ها</Typography>
 
             </Box>
-            <List component="nav" sx={{overflow:"scroll"}}>
+            <List component="nav" sx={{overflow: "scroll"}}>
                 {AssortmentCardData.map(item => (
-                    <ListItem button key={item.titleEn} sx={{ display:"flex", flexDirection:"column",
-                        alignItems:"flex-start", textAlign:"left"}}>
+                    <ListItem button
+                              key={item.titleEn}
+                              sx={{
+                                  display: "flex", flexDirection: "column",
+                                  alignItems: "flex-start", textAlign: "right"
+                              }}
+                    >
+                        <Box
+                            sx={{
+                                display: "flex", flexDirection: "row", gap:'5px' ,
+                                alignItems: "center", justifyContent:'center' , textAlign: "right"
+
+                            }}
+                        >
                         <Lens sx={{color: "lightblue", fontSize: "0.5rem"}}/>
                         <ListItemText primary={item.title}/>
-                        {
-                            getSubCategory(item.titleEn).map(group => (
-                                <p sx={{display:'flex' , fontSize:'0.5rem' , color:'gray'}}
-                                    key={group.id}
+                        </Box>
+                        <Box
+                            sx={{display: 'block' , gap:'5px'}}
 
-                                > {group.name} , </p>
-                            ))}
+                        >
+
+                            {
+                                getSubCategory(item.titleEn).splice(0, 3).map(group => (
+
+                                    <Typography
+                                        key={group.id}
+                                        variant="h5" component="h5"
+                                        sx={{display: 'inline', fontSize: '0.7rem', color: 'gray'}}
+                                    >
+                                        {`${group.name} , `}
+                                    </Typography>
+
+                                ))
+                            }
+                            <Typography
+                                variant="h5" component="h5"
+                                sx={{display: 'inline', fontSize: '0.7rem', color: 'gray'}}
+                            > ... </Typography>
+
+                        </Box>
                         {/*<ListItemText primary={"item"}/>*/}
                     </ListItem>
                 ))}
