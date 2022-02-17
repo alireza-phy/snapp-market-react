1081d085ffce29c78b67738cf2653b3de0cd6ee9
Abolfazl-drx <abolfazl_drx@yahoo.com>
2022-02-17 08:25:21 -0800

    Algorithm of page4 added ,brand filter box and type filter box are working.

--- components/Page4/Page4.jsx
+++ components/Page4/Page4.jsx
@@ -4,40 +4,114 @@ import PaginationAndSort from '../Pagination/Pagination'
 import Grid from '@mui/material/Grid'
 import Container from '@mui/material/Container'
 import ProductData from '../ProductData/ProductData'
-import Slider from '@mui/material/Slider'
 import {useState , useEffect} from "react";
 
 const Page4 = ({groupId , categoryName , searchList}) => {
-
-    const [data, setData] = (searchList) ? useState(searchList) : useState(ProductData.filter((dairy) => (dairy.categoryEn == categoryName && dairy.groupId == groupId)))
+    const [data, setData] = (searchList) ? useState(searchList) : useState(ProductData.filter((dairy) => (dairy.categoryEn === categoryName && dairy.groupId === groupId)))
     const [productData, setProductData] = useState([...data])
-    const [filter, setFilter] = useState([])
-    const [justDiscountState, setJustDiscountState] = useState(false)
+    const [filterBrandAndTypes, setFilterBrandAndTypes] = useState([])
+    const [avaibale, setAvaible] = useState(false)
+    const [discount, setDiscount] = useState(false)
+    const getAllBrandsAndAllTypesOfCategories = () => {
+        let brandsOfCategoriesPe = []
+        let brandsOfCategoriesEn = []
+        let tagsOfCategories = []
+        let result01 = []
+        let result02 = []
+        data.map(item => {
+            if (!(brandsOfCategoriesPe).includes(item.brand.brandPe)) {
+                brandsOfCategoriesPe.push(item.brand.brandPe)
+                brandsOfCategoriesEn.push(item.brand.brandEn)
+            }
+        })
+        data
+            .filter((tag) => (
+                tag.tags.length !== 0
+            ))
+            .map((tags) => (tags.tags.forEach((tag) => {
+                if (!(tagsOfCategories.includes(tag.name))) {
+                    tagsOfCategories.push(tag.name)
+                }
+            })))
+        tagsOfCategories
+            .map((item) =>
+                result02.push({tagName: item, status: false}
+                ))
+        data.map(product => result02.map(type => (type.tagName === product.tags.includes((tag) => tag.name)) ?
+            {...type, tagName: product.tags.includes((tag) => tag.name)} : null))
+        brandsOfCategoriesPe.map((item, index) =>
+            result01.push({brandPe: item, brandEn: brandsOfCategoriesEn[index], status: false}
+            ))
+        data.map(product => result01.map(brand => (brand.brandPe === product.brand.brandPe) ?
+            {...brand, brandEn: product.brand.brandEn} : null))
+
+        for (let item of result01) {
+            if (filterBrandAndTypes.includes(item.brandPe)) item.status = true
+        }
+        for (let item of result02) {
+            if (filterBrandAndTypes.includes(item.tagName)) item.status = true
+        }
+        return [result01, result02]
+    }
+    let allBrandsAndAllTypesOfCategories = getAllBrandsAndAllTypesOfCategories()
+
+    const removeFilterList = () => {
+        setFilterBrandAndTypes([])
+    }
+    const defaults = () => {
+        setProductData([...data])
+    }
+    const maxDiscount = () => {
+        setProductData([...productData].sort((a, b) => a.discount <= b.discount ? 1 : -1))
+    }
+
+    const maxDiscountAmount = () => {
+        setProductData([...productData].sort((a, b) =>
+            a.price - a.price * (1 - (a.discount / 100)) <= b.price - b.price * (1 - (b.discount / 100)) ? 1 : -1))
+    }
+    const inexpensiveHandler = () => {
+        setProductData([...productData].sort((a, b) => a.price * (1 - (a.discount / 100)) >= b.price * (1 - (b.discount / 100)) ? 1 : -1))
+    }
+
+    const expensiveHandler = () => {
+        setProductData([...productData].sort((a, b) => a.price * (1 - (a.discount / 100)) <= b.price * (1 - (b.discount / 100)) ? 1 : -1))
+    }
+    useEffect(() => {
+        if (filterBrandAndTypes.length > 0) {
+            const found1 = allBrandsAndAllTypesOfCategories[1].map((tag) => tag.tagName).some(r => filterBrandAndTypes.indexOf(r) >= 0)
+            const found2 = allBrandsAndAllTypesOfCategories[0].map((tag) => tag.brandPe).some(r => filterBrandAndTypes.indexOf(r) >= 0)
+            if (found1 && found2) {
+                setProductData([...data]
+                    .filter((products) => products.tags.length !== 0)
+                    .filter((products) => filterBrandAndTypes.includes(products.brand.brandPe))
+                    .filter((products) => products.tags.some((tag)=> filterBrandAndTypes.includes(tag.name)))
+                )
+            } else if (found1) {
+                setProductData([...data]
+                    .filter((products) => products.tags.length !== 0)
+                    .filter((products) => products.tags.some((tag) => filterBrandAndTypes.includes(tag.name)))
+                )
+            } else if (found2) {
+                setProductData([...data].filter((products) => filterBrandAndTypes.includes(products.brand.brandPe)))
+            }
+        } else {
+            setProductData([...data])
+        }
+        if (filterBrandAndTypes.includes('فقط کالاهای موجود')) {
+            setAvaible(true)
+            setProductData([...data].filter((product) => product.inventory.available === true))
+        } else if (filterBrandAndTypes.includes('فقط کالاهای تخفیف دار')) {
+            setDiscount(true)
+            setProductData([...data].filter((product) => product.discount > 0))
+        }
+    }, [filterBrandAndTypes]);
+    console.log(filterBrandAndTypes)
     const allPrices = data.map((product) => (
         product.price * (1 - (product.discount / 100))
     ))
     const lowPrice = Math.min(...allPrices)
     const highPrice = Math.max(...allPrices)
     const [ranger, setRange] = useState([lowPrice, highPrice])
-
-    const rangeHandler = (e, newValue) => {
-        setRange(newValue)
-        setProductData([...data].filter((product) => (
-            product.price >= newValue[0] && product.price <= newValue[1]
-        )))
-    }
-    const justDiscount = (e) => {
-        setJustDiscountState(!justDiscountState)
-        if (justDiscountState) {
-            setProductData([...data])
-        } else {
-            setProductData([...productData].filter((product) => (
-                product.discount > 0
-            )))
-        }
-
-    }
-
     const priceRange = {
         to: ranger[0],
         from: ranger[1]
@@ -46,51 +120,34 @@ const Page4 = ({groupId , categoryName , searchList}) => {
         {id: 1, name: 'شیر تازه'},
         {id: 2, name: 'شیر مدت دار'}
     ]
-    const allBrandsFromData = data.map((product) => (
-        {en: product.brand.brandEn, pe: product.brand.brandPe}
-    ))
-    const removeDubBrands = allBrandsFromData
-        .filter((v, i, a) => a.findIndex(t => ['en', 'pe'].every(k => t[k] === v[k])) === i)
-    const brands = removeDubBrands.map((product) => ({id: Math.floor(Math.random() * 1000), ...product}))
-    const emptyArrayForTypes = []
-    const typesFiltered = data.map((product) => (product.tags))
-        .filter((tag) => (tag.length !== 0))
-        .map((product) => (product.forEach((tag) => (emptyArrayForTypes.push(tag)))))
-    const removeIdTypes = emptyArrayForTypes.map((tag) => (tag.name))
-    const types = [...new Set(removeIdTypes)]
-        .map((tag) => ({tag}))
-        .map((tag) => ({id: Math.floor(Math.random() * 1000), ...tag}))
-    const filterRemoveHandler = (id) => {
-        setFilter(filter.filter((tag) => tag.id !== id))
-    }
-    const removeFilterList = () => {
-        setFilter([])
-    }
-    const productFilter = () => {
-    }
-    console.log(data)
     return (
         <Container sx={{px: {xs: 0, sm: '24px'}}}>
             <Grid container sx={{direction: 'rtl', mt: 4}} columnSpacing={{xs: 0, sm: 0, md: 3, lg: 3, xl: 3}}>
                 <ProductsFilter
                     categories={categories}
-                    brands={brands}
-                    types={types}
-                    filter={filter}
+                    brands={allBrandsAndAllTypesOfCategories[0]}
+                    types={allBrandsAndAllTypesOfCategories[1]}
+                    filterBrandAndTypes={filterBrandAndTypes}
+                    setFilterBrandAndTypes={setFilterBrandAndTypes}
                     priceRange={priceRange}
                     ranger={ranger}
                     lowPrice={lowPrice}
                     highPrice={highPrice}
-                    filterRemoveHandler={filterRemoveHandler}
                     removeFilterList={removeFilterList}
-                    handleChange={rangeHandler}
-                    justDiscount={justDiscount}
-                    justDiscountState={justDiscountState}
+                    avaibale={avaibale}
+                    discount={discount}
+                    setAvaible={setAvaible}
+                    setDiscount={setDiscount}
+
                 />
                 <Grid item xs={12} sm={12} md={9} xl={9}>
-                    {/*<Breadcrumb productCounter={data.length} category={data[0].categoryPe} group={data[0].groupName}/>*/}
-                    <PaginationAndSort productData={productData}/>
-
+                    <Breadcrumb productCounter={productData.length} category={data[0].categoryPe} group={data[0].groupName}/>
+                    <PaginationAndSort defaults={defaults}
+                                       maxDiscount={maxDiscount}
+                                       maxDiscountAmount={maxDiscountAmount}
+                                       inexpensiveHandler={inexpensiveHandler}
+                                       expensiveHandler={expensiveHandler}
+                                       productData={productData}/>
                 </Grid>
             </Grid>
         </Container>
